f3802ddfec6fa979f7f83f6046c2dff1f450dce8
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-06-19 14:54:21 -0500

    custom types

---
+++ 03 Defining Custom Types/index.ts
@@ -0,0 +1,10 @@
+type Food = string
+// the type keyword allows you to create custom types. 
+
+let favoriteFood: Food = "pizza"
+
+let person2 = {
+    name: "Jill",
+    age: 66, 
+    isStudent: false
+}
