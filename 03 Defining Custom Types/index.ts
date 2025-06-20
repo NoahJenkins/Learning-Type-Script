f2c5e77c4cd6f9c4fa624fce8638d7d18d2a7d5a
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-06-20 15:09:44 -0500

    function

--- 03 Defining Custom Types/index.ts
+++ 03 Defining Custom Types/index.ts
@@ -1,18 +1,27 @@
+console.log('Script started');
+
 type Food = string
 // the type keyword allows you to create custom types. 
 
 let favoriteFood: Food = "pizza"
 
+type Address = {
+        street: string,
+        city: string, 
+        country: string
+    }
+
 type Person = {
     name: string,
     age: number,
     isStudent: boolean
+    address?: Address // note that the address variable in the Person type is option, see how it is not in person 2, but is in person 3. 
 }
 
 let person1 = {
     name: "Jill",
     age: 66, 
-    isStudent: false
+    isStudent: false 
 }
 
 let person2: Person = {
@@ -20,3 +29,21 @@ let person2: Person = {
     age: 33,
     isStudent: true
 }
+
+let person3: Person = {
+    name:"John",
+    age: 33,
+    isStudent: true,
+    address: {
+        street: "street",
+        city: "city", 
+        country: "USA! USA! USA!"
+    }
+}
+
+function displayInfo(person: Person) {
+    console.log(person3.name)
+    console.log(`${person3.name} lives at ${person3.address!.street}`)
+}
+
+displayInfo(person3)
